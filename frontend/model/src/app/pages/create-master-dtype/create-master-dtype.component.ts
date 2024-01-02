5c7cc8ffaef97f7156c9dd232700a2444ab25821
NaolGB <naolgbasaye@gmail.com>
2024-01-02 07:13:25 +0000

    master data att attribute componenet completed

--- frontend/model/src/app/pages/create-master-dtype/create-master-dtype.component.ts
+++ frontend/model/src/app/pages/create-master-dtype/create-master-dtype.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormArray, FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
 	selector: 'app-create-master-dtype',
@@ -10,10 +10,22 @@ import { FormArray, FormBuilder, ReactiveFormsModule, Validators } from '@angula
 	styleUrl: './create-master-dtype.component.css'
 })
 export class CreateMasterDtypeComponent {
+	dtypeOptions = [
+		{typeName: 'string'},
+		{typeName: 'number'},
+		{typeName: 'datetime'},
+		{typeName: 'boolean'},
+	]
+
 	masterDtypeForm = this.formBuilder.group({
-		name: ['', Validators.required],
+		nameAndType: this.formBuilder.group({
+			name: ['', Validators.required],
+		}),
 		extraAttributes: this.formBuilder.array([
-			this.formBuilder.control('')
+			this.formBuilder.group({
+				name: ['', Validators.required],
+				dtype: [this.dtypeOptions[0], Validators.required]
+			}),
 		])
 	})
 
@@ -21,5 +33,33 @@ export class CreateMasterDtypeComponent {
 
 	get extraAttributes() {
 		return this.masterDtypeForm.get("extraAttributes") as FormArray
+	}
+
+	addAttribute() {
+		this.extraAttributes.push(
+			this.formBuilder.group({
+				name: ['', Validators.required],
+				dtype: [this.dtypeOptions[0], Validators.required]
+			}),
+		)
+	}
+
+	onSubmit() {
+		interface ParsedPostData {
+			[key: string]: string
+		}
+		const parsedPostData: ParsedPostData = {
+			name: this.masterDtypeForm.get("nameAndType")?.get("name")?.value || ""
+		}
+
+		for (let i = 0; i < this.extraAttributes.length; i++) {
+			let attName: string = this.extraAttributes.at(i).value["name"]
+			if(attName != null) {
+				const dtypeTypeName: string = this.extraAttributes.at(i).value["dtype"]["typeName"];
+				parsedPostData[attName] = dtypeTypeName
+			}
+		}
+
+		console.log(parsedPostData)
 	}
 }
