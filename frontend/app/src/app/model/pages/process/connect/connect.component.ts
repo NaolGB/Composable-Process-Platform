5379f99719c9293c7006440fc9b3403902604a1a
NaolGB <naolgbasaye@gmail.com>
2024-01-14 20:47:10 +0000

    added guide for determining rows and columns for steps

--- frontend/app/src/app/model/pages/process/connect/connect.component.ts
+++ frontend/app/src/app/model/pages/process/connect/connect.component.ts
@@ -50,9 +50,49 @@ export class ConnectComponent {
   }
 
   getStepsOrder() {
+    /**
+     * Sorts steps into rows and columns for process preview. 
+     * Step order is determined based on their next_step values. 
+     *  - steps that are in other steps' next_step have a higher colimn than their source step 
+     *    -- (eg: if step b is in step a's next_step object, step b's column is one after step a's)
+     *  - steps that come from the same step (eg: two steps in another step's next_step object) have the same column
+     *  - steps that share next_step (eg: two steps that have at least one common next_step value) have the same column
+     * 
+     * stepOrder is an object that holds the step name (which is used as it's key) and order information. It reffers to 
+     * the process data with 1-to-1 binding and has the following structure.
+     * {
+     *  a: {
+     *        row: 0,
+     *        column: 0
+     *        next_steps: {b, c}
+     *      }
+     *  b: {
+     *        row: 0,
+     *        column: 1
+     *        next_steps: {}
+     *      }
+     *  c: {
+     *        row: 0,
+     *        column: 0
+     *        next_steps: {}
+     *      }
+     * }
+     * 
+     * stepOrder's rows and columns are determined with the following method.
+     * 1. determine the connected adn unconnected steps
+     * 2. for the connected steps, find the start 
+     *    - using any first step, traverse through connected steps until you find a step not in any step's next_step
+     * 3. set the first step's column as 0
+     *    - for all steps that share a next_step with the first step set their column as 0
+     *    - after completing the first column, assign each one a unique row
+     * 4. repeat step 4 until all connected steps are assigned row and column
+     */
     for (const key in this.processData['attributes']['steps']) {
       this.stepOrder.push([key])
     }
+
+
+    
   }
 
 }
