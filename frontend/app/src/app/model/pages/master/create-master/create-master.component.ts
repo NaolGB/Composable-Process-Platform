94d960ecbb84017ae8b26de9537390dea3d0e4b9
NaolGB <naolgbasaye@gmail.com>
2024-02-13 06:37:48 +0000

    create master data restructured

--- frontend/app/src/app/model/pages/master/create-master/create-master.component.ts
+++ frontend/app/src/app/model/pages/master/create-master/create-master.component.ts
@@ -6,71 +6,85 @@ import { MasterDataTypeInterface } from '../../../../interfaces';
 @Component({
   selector: 'app-create-master',
   templateUrl: './create-master.component.html',
-  styleUrl: './create-master.component.css'
+  styleUrl: './create-master.component.css',
 })
 export class CreateMasterComponent {
-  dtypeOptions = [ 'string', 'number', 'datetime', 'boolean']
-  showSidebar: boolean = false
+  dtypeOptions = ['string', 'number', 'datetime', 'boolean'];
+  showSidebar: boolean = false;
+  showPreviewSection: boolean = true;
 
-	masterDtypeForm = this.formBuilder.group({
-		nameAndType: this.formBuilder.group({
-			name: ['', Validators.required],
-		}),
-		extraAttributes: this.formBuilder.array([
-			this.formBuilder.group({
-				name: ['', Validators.required],
-				dtype: [this.dtypeOptions[0], Validators.required]
-			}),
-		])
-	})
+  masterDtypeForm = this.formBuilder.group({
+    nameAndType: this.formBuilder.group({
+      name: ['', Validators.required],
+    }),
+    extraAttributes: this.formBuilder.array([
+      this.formBuilder.group({
+        name: ['', Validators.required],
+        dtype: [this.dtypeOptions[0], Validators.required],
+      }),
+    ]),
+  });
 
-	masterDtypeInDB: string[] = []
+  masterDtypeInDB: string[] = [];
 
-	constructor(private formBuilder: FormBuilder, private apiServices: DataService) {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private apiServices: DataService
+  ) {}
 
-	ngOnInit() {
-		// sidebar
-		this.apiServices.getMasterDtypeIds().subscribe(
-			(response) => {
-				this.masterDtypeInDB = response['ids']
-			}
-		)
+  ngOnInit() {
+    // sidebar
+    this.apiServices.getMasterDtypeIds().subscribe((response) => {
+      this.masterDtypeInDB = response['ids'];
+    });
+  }
+
+  get extraAttributes() {
+    return this.masterDtypeForm.get('extraAttributes') as FormArray;
+  }
+
+  addAttribute() {
+    this.extraAttributes.push(
+      this.formBuilder.group({
+        name: ['', Validators.required],
+        dtype: [this.dtypeOptions[0], Validators.required],
+      })
+    );
+  }
+
+  toggleSidebar(): void {
+    this.showSidebar = !this.showSidebar;
+  }
+
+  togglePreviewSection(): void {
+    this.showPreviewSection = !this.showPreviewSection;
+  }
+
+  onSubmit() {
+    const parsedPostData: MasterDataTypeInterface = {
+      name: this.masterDtypeForm.get('nameAndType')?.get('name')?.value || '',
+      _id: '',
+      organization: '',
+      attributes: {},
+    };
+
+    for (let i = 0; i < this.extraAttributes.length; i++) {
+      let attName: string = this.extraAttributes.at(i).value['name'];
+      if (attName != null) {
+        const dtypeTypeName: string = this.extraAttributes.at(i).value['dtype'];
+        parsedPostData.attributes[attName] = dtypeTypeName;
+      }
+    }
+
+    this.apiServices.postMasterDtype(parsedPostData).subscribe();
+  }
+
+	// Style functions
+	getMainSectionHeight(): string {
+		return this.showPreviewSection ? '60vh' : '90vh';
 	}
 
-	get extraAttributes() {
-		return this.masterDtypeForm.get("extraAttributes") as FormArray
-	}
-
-	addAttribute() {
-		this.extraAttributes.push(
-			this.formBuilder.group({
-				name: ['', Validators.required],
-				dtype: [this.dtypeOptions[0], Validators.required]
-			}),
-		)
-	}
-
-	toggleSidebar(): void {
-		this.showSidebar = !this.showSidebar;
-	}
-
-  	onSubmit() {
-		
-		const parsedPostData: MasterDataTypeInterface = {
-			name: this.masterDtypeForm.get("nameAndType")?.get("name")?.value || "",
-			_id: '',
-			organization: '',
-			attributes: {}
-		}
-
-		for (let i = 0; i < this.extraAttributes.length; i++) {
-			let attName: string = this.extraAttributes.at(i).value["name"]
-			if(attName != null) {
-				const dtypeTypeName: string = this.extraAttributes.at(i).value["dtype"];
-				parsedPostData.attributes[attName] = dtypeTypeName
-			}
-		}
-
-		this.apiServices.postMasterDtype(parsedPostData).subscribe()
+	getPreviewSectionHeight(): string {
+		return this.showPreviewSection ? '40vh' : '5vh';
 	}
 }
