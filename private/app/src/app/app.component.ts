8f9a386cb629f66bdf43a20338f66ec43207074c
NaolGB <naolgbasaye@gmail.com>
2024-03-06 18:29:52 +0000

    updated layout

--- private/app/src/app/app.component.ts
+++ private/app/src/app/app.component.ts
@@ -1,17 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  showSidebar = false;
+  title = 'app';
+  isSidebarOpen = false;
 
-  toggleSidebar(): void {
-    this.showSidebar = !this.showSidebar;
+  toggleSidebar() {
+    this.isSidebarOpen = !this.isSidebarOpen;
   }
 }
