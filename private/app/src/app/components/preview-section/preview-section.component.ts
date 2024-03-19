51a320400565e48fea473a1e578ad41d50d8a159
Naol Basaye <57215685+NaolGB@users.noreply.github.com>
2024-03-19 21:05:00 +0000

    added style pattern to preview section

--- private/app/src/app/components/preview-section/preview-section.component.ts
+++ private/app/src/app/components/preview-section/preview-section.component.ts
@@ -8,22 +8,5 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
   styleUrl: './preview-section.component.scss'
 })
 export class PreviewSectionComponent {
-  @ViewChild('zoomContainer') zoomContainer!: ElementRef;
-
-  zoomLevel: number = 1;
-
-  constructor() { }
-
-  ngOnInit() {
-    this.zoomContainer.nativeElement.addEventListener('wheel', (event: WheelEvent) => {
-      event.preventDefault(); // Prevent default scrolling behavior
-      const zoomDelta = event.deltaY > 0 ? -0.1 : 0.1; // Adjust as needed
-      this.zoomLevel = Math.max(0.1, Math.min(this.zoomLevel + zoomDelta, 3)); // Limit zoom level
-      this.updateZoom();
-    });
-  }
-
-  private updateZoom() {
-    this.zoomContainer.nativeElement.style.transform = `scale(${this.zoomLevel})`;
-  }
+  
 }
