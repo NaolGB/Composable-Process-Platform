55f5c96e85b337593ffb83dff5afb50f24b00ba7
Naol Basaye <57215685+NaolGB@users.noreply.github.com>
2024-04-02 18:15:46 +0100

    configured firebase auth

--- private/app/src/app/app.config.ts
+++ private/app/src/app/app.config.ts
@@ -1,14 +1,35 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient } from '@angular/common/http';
 
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAKOKato4_tv2e5s2tNPZSGXAWCimGEupM",
+  authDomain: "project-x2-0.firebaseapp.com",
+  projectId: "project-x2-0",
+  storageBucket: "project-x2-0.appspot.com",
+  messagingSenderId: "689501773952",
+  appId: "1:689501773952:web:cddae3be72052a76104665",
+  measurementId: "G-ZM2SLWJM35"
+};
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes), 
     provideClientHydration(), 
-    provideHttpClient()
+    provideHttpClient(),
+    importProvidersFrom([
+      provideFirebaseApp(() => initializeApp(firebaseConfig)),
+      provideAuth(() => getAuth())
+    ])
   ]
 };
+
